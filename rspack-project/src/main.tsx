b044b5fe7c7b10771cb4888aedf2a4765ef32e09
Collin Brown <collin.j.brown95@gmail.com>
2023-08-23 13:17:02 -0400

    feat(src): Include modified Inbox example from Lingui-js docs

--- rspack-project/src/main.tsx
+++ rspack-project/src/main.tsx
@@ -1,10 +1,29 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import { createRoot } from "react-dom/client";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+import { i18n } from "@lingui/core";
+import { I18nProvider } from "@lingui/react";
+import { messages } from "./locales/en/messages";
+
+// import { Trans } from "@lingui/react";
+import { Trans } from "@lingui/macro";
+import Inbox from "./Inbox";
+
+i18n.load("en", messages);
+i18n.activate("en");
+
+const App = () => (
+  <I18nProvider i18n={i18n}>
+    {/* <p><Trans id={"nwR43V"} message="Attachment {name} saved" values={{ name }} /></p> */}
+    <p><Trans>test123</Trans></p>
+    <Inbox />
+  </I18nProvider>
+);
+
+const container = document.getElementById('root');
+const root = createRoot(container!); // createRoot(container!) if you use TypeScript
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
 )
